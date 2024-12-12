b10d354e75740d55c3c4564beb4fa17c22f7d56a
mdeepaktiwari <deepak.tiwari@kommunicate.io>
2024-12-12 19:15:38 +0530

    fix: add motion to image

--- components/Image.js
+++ components/Image.js
@@ -1,13 +1,22 @@
+import { motion } from "framer-motion";
+
 export const Image = ({ src, alt, additionalClass = "" }) => {
-  if (!src) return null;
   return (
-    <img
-      width="500px"
-      height="500px"
-      src={src}
-      loading="lazy"
-      className={`w-[52vh] ml-[calc(-26vh+156px)] max-w-[29rem] min-w-[18rem] ${additionalClass}`}
-      alt={alt}
-    />
+    <>
+      {src && (
+        <motion.img
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          width="500px"
+          height="500px"
+          src={src}
+          loading="lazy"
+          className={`w-[52vh] ml-[calc(-26vh+156px)] max-w-[29rem] min-w-[18rem] ${additionalClass}`}
+          alt={alt}
+        />
+      )}
+    </>
   );
 };
